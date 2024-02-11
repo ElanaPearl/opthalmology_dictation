ad19a0a1377eff35a4acafb190e8bbcf8651a6b7
Elana Simon <elanasimon95@gmail.com>
2024-02-10 22:25:39 -0800

    docstrings and save output

--- identify_keywords_in_text.py
+++ identify_keywords_in_text.py
@@ -1,13 +1,20 @@
 """ This script identifies the top n keywords in a document using the TF-IDF method. """
 
+import time
+
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def fit_tfidf_on_background_data(corpus: list[str]) -> TfidfVectorizer:
     """Fit a TF-IDF vectorizer on the background data."""
+    print("Fitting TF-IDF vectorizer on background data. This may take a minute")
+
+    s = time.time()
     vectorizer = TfidfVectorizer()
     vectorizer.fit(corpus)
+    e = time.time()
+    print(f"TF-IDF vectorizer fit in {e - s:.2f} seconds")
     return vectorizer
 
 
@@ -18,6 +25,7 @@ def find_top_n_words_in_document(
 
     The top words are determined by the sum of the TF-IDF scores for each word in the document.
     """
+    print(f"Finding top {n} keywords in document")
     tf_idf_scores = vectorizer.transform(document)
 
     tf_idf_per_word = pd.DataFrame(
@@ -31,7 +39,10 @@ def read_in_background_data(
     background_path: str,
 ) -> list[str]:
     """Read in the background data assume to be a CSV file with a column named 'TEXT'."""
+
+    print(f"Reading in background data from {background_path}")
     data = pd.read_csv(background_path)
+    print(f"Read in {len(data):,} rows of background data")
 
     if "TEXT" not in data.columns:
         raise ValueError(
@@ -64,5 +75,19 @@ def find_keywords(
     return find_top_n_words_in_document(vectorizer, document, n_keywords)
 
 
+def main(
+    output_path: str = "data/keywords.txt",
+    background_path: str = "data/background_tf_idf.csv",
+    document_path: str = "data/dictation_long.txt",
+    n_keywords: int = 25,
+):
+    keywords = find_keywords()
+
+    print(f"Writing keywords to {output_path}")
+    with open(output_path, "w") as file:
+        for keyword in keywords:
+            file.write(f"{keyword}\n")
+
+
 if __name__ == "__main__":
-    print(find_keywords())
+    main()
